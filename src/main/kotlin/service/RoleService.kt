5a60267d4cef6758a5f42356644a2f3f99b68dbb
ihateblueb <ihateblueb@proton.me>
2025-05-19 12:44:48 -0400

    userHasRole on RoleService

--- src/main/kotlin/service/RoleService.kt
+++ src/main/kotlin/service/RoleService.kt
@@ -3,10 +3,13 @@ package me.blueb.service
 import me.blueb.db.entity.RoleEntity
 import me.blueb.db.suspendTransaction
 import me.blueb.db.table.RoleTable
+import me.blueb.db.table.UserTable
 import org.jetbrains.exposed.sql.Op
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 
 class RoleService {
+	private val userService = UserService()
+
 	suspend fun get(where: Op<Boolean>): RoleEntity? = suspendTransaction {
 		RoleEntity
 			.find { where }
@@ -14,4 +17,15 @@ class RoleService {
 	}
 
 	suspend fun getById(id: String): RoleEntity? = get(RoleTable.id eq id)
+
+	suspend fun userHasRole(userId: String, roleId: String): Boolean {
+		val user = userService.getById(userId)
+
+		if (user != null && user.roles.contains(roleId))
+			return true
+
+		return false
+	}
+
+	// todo: userHasRoleType with Admin,Mod
 }
