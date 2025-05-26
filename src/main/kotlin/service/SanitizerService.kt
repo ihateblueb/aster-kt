9cd4476c4bc7f84d0903df9ffa2e0a7619a206cf
ihateblueb <ihateblueb@proton.me>
2025-05-26 17:31:08 -0400

    backend: html sanitizer

--- src/main/kotlin/service/SanitizerService.kt
+++ src/main/kotlin/service/SanitizerService.kt
@@ -1,13 +1,18 @@
 package me.blueb.service
 
 import org.apache.commons.text.StringEscapeUtils
+import org.owasp.html.PolicyFactory
+import org.owasp.html.Sanitizers
 
 class SanitizerService {
+	val policy: PolicyFactory = Sanitizers.FORMATTING
+		.and(Sanitizers.LINKS)
+		.and(Sanitizers.BLOCKS)
+
 	fun sanitize(string: String, escape: Boolean = false): String {
 		if (escape)
 			return StringEscapeUtils.escapeHtml4(string)
 
-		// todo: https://github.com/OWASP/java-html-sanitizer
-		return string
+		return policy.sanitize(string)
 	}
 }
